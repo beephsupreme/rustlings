c5c3e97c11ed5d9ce2dac746eac34f30ecc07eeb
Michael Rowsey <mike@rowsey.org>
2023-07-27 15:30:04 -0700

    exercises up to 52

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,8 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+#[derive(Debug, Clone, Copy)]
 struct Point {
     x: i32,
     y: i32,
@@ -15,7 +14,7 @@ fn main() {
 
     match y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
-        _ => panic!("no match!"),
+        None => panic!("no match!"),
     }
     y; // Fix without deleting this line.
 }
